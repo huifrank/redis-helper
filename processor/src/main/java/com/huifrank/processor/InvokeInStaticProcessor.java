f922609e773bf8e6d5dac87884b79b52f1b0f62d
huifrank <huifrank01@gmail.com>
2020-08-16 21:59:53 +0800

    anno Processor

--- processor/src/main/java/com/huifrank/processor/InvokeInStaticProcessor.java
+++ processor/src/main/java/com/huifrank/processor/InvokeInStaticProcessor.java
@@ -3,13 +3,22 @@ package com.huifrank.processor;
 import com.google.auto.service.AutoService;
 import com.google.common.collect.ImmutableSet;
 import com.huifrank.common.util.invoker.InvokeInStatic;
+import org.eclipse.jdt.core.dom.AST;
+import org.eclipse.jdt.core.dom.ASTParser;
+import org.eclipse.jdt.internal.compiler.ASTVisitor;
+import org.eclipse.jdt.internal.compiler.apt.dispatch.BaseProcessingEnvImpl;
+import org.eclipse.jdt.internal.compiler.apt.model.Factory;
+import org.eclipse.jdt.internal.compiler.apt.model.VariableElementImpl;
 
 
 import javax.annotation.processing.*;
 import javax.lang.model.SourceVersion;
+import javax.lang.model.element.AnnotationValue;
 import javax.lang.model.element.Element;
 import javax.lang.model.element.TypeElement;
 import javax.lang.model.util.Elements;
+import java.util.Arrays;
+import java.util.Map;
 import java.util.Set;
 
 @AutoService(InvokeInStatic.class)
@@ -20,6 +29,7 @@ public class InvokeInStaticProcessor extends AbstractProcessor {
     private Messager messager;
     private Elements elementUtils;
     private Filer filer;
+    private ProcessingEnvironment processingEnv;
 
 
     @Override
@@ -28,6 +38,7 @@ public class InvokeInStaticProcessor extends AbstractProcessor {
         messager = processingEnv.getMessager();
         elementUtils = processingEnv.getElementUtils();
         filer = processingEnv.getFiler();
+        this.processingEnv = processingEnv;
     }
     @Override
     public Set<String> getSupportedAnnotationTypes() {
@@ -46,7 +57,13 @@ public class InvokeInStaticProcessor extends AbstractProcessor {
     public boolean process(Set<? extends TypeElement> annotations, RoundEnvironment roundEnv) {
 
         Set<? extends Element> elementsAnnotatedWith = roundEnv.getElementsAnnotatedWith(InvokeInStatic.class);
+        elementsAnnotatedWith.forEach(ele->{
 
-        return true;
+
+
+
+        });
+
+        return false;
     }
 }
